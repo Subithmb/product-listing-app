83016ceb3698b38f1c7a7bf788275e4f8d1ed5dd
subith <bijusubith@gmail.com>
2023-10-12 08:06:12 +0530

    productlisting api created

--- Server/controllers/ProductController.js
+++ Server/controllers/ProductController.js
@@ -63,9 +63,259 @@ const ProductDatas=async(req,res)=>{
 }
 
 
+// //......................
+
+  
+async function findSubcategoriesAndProducts(categoryId, depth = 0, parentProducts = new Set()) {
+    try {
+      const categoryInfo = {
+        name: '',
+        products: new Set(), 
+        subcategories: [],
+        totalProductCount: 0,
+      };
+  
+      const subcategories = await Category.find({ parent: categoryId });
+  
+      for (const subcategory of subcategories) {
+        const subcategoryInfo = {
+          name: subcategory.name,
+          subcategories: [],
+          products: new Set(), 
+          totalProductCount: 0,
+        };
+  
+        const subcategorielist = await Category.find({ parent: subcategory._id });
+  
+        if (subcategorielist.length === 0) {
+          const products = await product.find({ category: subcategory._id });
+  
+          for (const p of products) {
+            if (!parentProducts.has(p._id)) {
+              subcategoryInfo.products.add(p); 
+              parentProducts.add({_id:p._id,name:p.name,category:p.category});
+            }
+          }
+  
+          subcategoryInfo.totalProductCount += subcategoryInfo.products.size;
+        }
+  
+        categoryInfo.subcategories.push(subcategoryInfo);
+  
+        const childCategoryInfo = await findSubcategoriesAndProducts(subcategory._id, depth + 1, parentProducts);
+        subcategoryInfo.subcategories = childCategoryInfo.subcategories;
+        subcategoryInfo.totalProductCount += childCategoryInfo.totalProductCount;
+  
+       
+        categoryInfo.totalProductCount += subcategoryInfo.totalProductCount;
+      }
+  
+      categoryInfo.name = categoryInfo.name || (await Category.findOne({ _id: categoryId })).name;
+  
+      return {
+        ...categoryInfo,
+        products: [...categoryInfo.products, ...parentProducts],
+      };
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+
+
+
+  
+  
+  // ...
+  
+  const findProductandCategory = async (req, res) => {
+    const id = req.query.id;
+    try {
+      const CategoryFound = await Category.findById(id).exec();
+      const result = await findSubcategoriesAndProducts(CategoryFound._id);
+      console.log(JSON.stringify(result, null, 2));
+      return res.status(201).json({result})
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+
+  //...............sample
+
+
+//   async function getProductsByCategory(req, res) {
+//     try {
+//         const categoryId = req.query.id;
+//         const products = new Set(); // Use a Set to avoid duplicate products
+//         const categoriesToCheck = [categoryId];
+//         const parentCategories = new Set();
+
+//         while (categoriesToCheck.length > 0) {
+//             const currentCategoryId = categoriesToCheck.pop();
+//             const category = await Category.findById(currentCategoryId);
+
+//             if (!category) {
+//                 continue;
+//             }
+
+//             const subcategories = await Category.find({ parent: currentCategoryId });
+//             const categoryProducts = await product.find({ category: currentCategoryId });
+
+//             if (categoryProducts.length > 0) {
+//                 parentCategories.add(currentCategoryId); // Add the parent category
+
+//                 // Check for duplicate products and add unique ones
+//                 categoryProducts.forEach((product) => {
+//                     products.add(product);
+//                 });
+//             }
+
+//             categoriesToCheck.push(...subcategories.map(subcategory => subcategory._id));
+//         }
+
+//         const categoryDetails = [];
+
+//         parentCategories.forEach(async(parentId) => {
+//             const parentCategory = await Category.findById(parentId);
+//             if (parentCategory) {
+//                 const subcategories = await Category.find({ parent: parentId });
+//                 categoryDetails.push({
+//                     _id: parentCategory._id,
+//                     name: parentCategory.name,
+//                     subcategories: subcategories.map(subcategory => ({
+//                         _id: subcategory._id,
+//                         name: subcategory.name
+//                     })),
+//                     products: [...products].filter(product => product.category.equals(parentId))
+//                 });
+//             }
+//         });
+
+//         return res.status(201).json({ products: [...products], categories: categoryDetails });
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+
+// async function getProductsByCategory(req, res) {
+//     try {
+//         const categoryId = req.query.id;
+//         const products = new Set(); 
+//         const categoriesToCheck = [categoryId];
+//         const categoryDetails = new Map();
+
+//         while (categoriesToCheck.length > 0) {
+//             const currentCategoryId = categoriesToCheck.pop();
+//             const category = await Category.findById(currentCategoryId);
+
+//             if (!category) {
+//                 continue;
+//             }
+
+//             const subcategories = await Category.find({ parent: currentCategoryId });
+//             const categoryProducts = await product.find({ category: currentCategoryId });
+
+//             // console.log(subcategories.length,';;;;;;;;;;');
+//             if (categoryProducts.length > 0 || subcategories.length>0 ) {
+                
+//                 categoryProducts.forEach((product) => {
+//                     products.add(product);
+//                 });
+               
+//                 categoryDetails.set(currentCategoryId, {
+//                     _id: category._id,
+//                     name: category.name,
+//                     subcategories: subcategories.map(subcategory => ({
+//                         _id: subcategory._id,
+//                         name: subcategory.name
+//                     }))
+//                 });
+//             }
+
+//             categoriesToCheck.push(...subcategories.map(subcategory => subcategory._id));
+//         }
+
+//         const categoryIdDetails = await Category.findById(categoryId);
+        
+//         return res.status(201).json({ categoryId: { _id: categoryIdDetails._id, name: categoryIdDetails.name }, products: [...products], categories: [...categoryDetails.values()] });
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+
+
+async function getProductsByCategory(req, res) {
+    try {
+        const categoryId = req.query.id;
+        const allProducts = new Set(); 
+        const categoriesToCheck = [categoryId];
+        const categoryDetails = new Map();
+
+        while (categoriesToCheck.length > 0) {
+            const currentCategoryId = categoriesToCheck.pop();
+            const category = await Category.findById(currentCategoryId);
+
+            if (!category) {
+                continue;
+            }
+
+            const subcategories = await Category.find({ parent: currentCategoryId });
+            const categoryProducts = await product.find({ category: currentCategoryId });
+
+           
+            const productCount = categoryProducts.length;
+
+            if (productCount > 0) {
+                categoryProducts.forEach((product) => {
+                    allProducts.add(product);
+                });
+
+
+                const parentCategory = await Category.findById(category.parent);
+
+
+                categoryDetails.set(currentCategoryId, {
+                    _id: category._id,
+                    name: category.name,
+                    productCount, 
+
+                    //parent finding
+                    parent: parentCategory ? { _id: parentCategory._id, name: parentCategory.name } : null,
+                    
+                    subcategories: subcategories.map(subcategory => ({
+                        _id: subcategory._id,
+                        name: subcategory.name
+                    }))
+                });
+            }
+
+            categoriesToCheck.push(...subcategories.map(subcategory => subcategory._id));
+        }
+
+        const categoryIdDetails = await Category.findById(categoryId);
+
+        return res.status(201).json({
+            categoryId: { _id: categoryIdDetails._id, name: categoryIdDetails.name },
+            totalProductCount: allProducts.size,
+            products: [...allProducts],
+            categories: [...categoryDetails.values()]
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+
+  
 
 module.exports={
     AddProduct,
-    ProductDatas
+    ProductDatas,
+    findProductandCategory,
+    getProductsByCategory
 
 }
--- Server/routes/AdminRoute.js
+++ Server/routes/AdminRoute.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const adminRouter=express.Router()
 const{Addcategory,Addsubcategory,categoryDatas,categoryDataforProduct,subcategoryfinding,subcategoryfindingtoAddproduct,categoryDataforsubcategory}=require('../controllers/categoryController')
-const{AddProduct,ProductDatas}=require('../controllers/ProductController')
+const{AddProduct,ProductDatas,findProductandCategory,getProductsByCategory}=require('../controllers/ProductController')
 
 adminRouter.post('/addCategory',Addcategory)
 adminRouter.post('/addsubcategory',Addsubcategory)
@@ -12,5 +12,7 @@ adminRouter.get('/products',ProductDatas)
 adminRouter.get('/categoryforProduct',categoryDataforProduct)
 adminRouter.get('/categoryforAddProduct',subcategoryfindingtoAddproduct)
 adminRouter.get('/subcategory',subcategoryfinding)
+adminRouter.get('/findProductandCategory',findProductandCategory)
+adminRouter.get('/getProductsByCategory',getProductsByCategory)
 
 module.exports=adminRouter
